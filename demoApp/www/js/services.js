13bcbc96a49d2807cc96a26d5f2226d4a4a9cee7
Rajkumar <rajkumar.k.msc@gmail.com>
2013-12-11 17:29:26 +0530

    changed by Rajkumar

--- demoApp/www/js/services.js
+++ demoApp/www/js/services.js
@@ -8,10 +8,9 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var pets = [
-    { id: 0, title: 'Cats', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    { id: 1, title: 'Dogs', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-    { id: 2, title: 'Turtles', description: 'Everyone likes turtles.' },
-    { id: 3, title: 'Sharks', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
+    { id: 0, title: 'Mr.Swami Venkat', description: 'CEO iNexGen games.' },
+    { id: 1, title: 'Mr.Ram', description: 'Project Manager, iNexGen games' },
+    { id: 2, title: 'Mr.Karthic', description: 'Project Manager, Beyond Universe Software Solutions.' }
   ];
 
   return {
