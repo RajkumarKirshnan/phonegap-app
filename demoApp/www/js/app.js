ee6bdfd60a0321dc5adc644afd79a31e90f7af8a
Rajkumar <rajkumar.k.msc@gmail.com>
2013-12-12 13:25:51 +0530

    barcode scanner added

--- demoApp/www/js/app.js
+++ demoApp/www/js/app.js
@@ -34,4 +34,14 @@ angular.module('starter', ['ionic', 'ngRoute', 'ngAnimate', 'starter.services', 
   });
 
 });
-
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
