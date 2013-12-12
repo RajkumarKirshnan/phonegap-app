9e87aa071f37714514a58a849cf4b97d125a3e3c
Rajkumar <rajkumar.k.msc@gmail.com>
2013-12-12 14:14:26 +0530

    source change

--- demoApp/www/js/app.js
+++ demoApp/www/js/app.js
@@ -34,14 +34,3 @@ angular.module('starter', ['ionic', 'ngRoute', 'ngAnimate', 'starter.services', 
   });
 
 });
-cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
---
+++ demoApp/www/js/barcodeapp.js
@@ -0,0 +1,9 @@
+		var function readBarcode(){
+		cordova.plugins.barcodeScanner.encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
+}
+document.addEventListener('DOMContentLoaded',readBarcode);
